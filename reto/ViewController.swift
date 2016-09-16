26574306a8a2443d38f7e15af1b13e8e22565db9
Mac Book Air <macbookair@macbook-air-de-mac.local>
2016-09-15 21:34:26 -0500

    Initial Commit

---
+++ reto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reto
+//
+//  Created by Mac Book Air on 15/09/16.
+//  Copyright © 2016 Mac Book Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ retoTests/retoTests.swift
@@ -0,0 +1,36 @@
+//
+//  retoTests.swift
+//  retoTests
+//
+//  Created by Mac Book Air on 15/09/16.
+//  Copyright © 2016 Mac Book Air. All rights reserved.
+//
+
+import XCTest
+@testable import reto
+
+class retoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
